ffc80955b658deca557bea87669432e9d82cf9c1
Radu Mereuta <headness13@gmail.com>
2019-08-06 14:26:30 +0300

    Fix RemoveBracket not eliminating singleton ambiguities

--- src/main/java/org/kframework/parser/concrete2kore/disambiguation/RemoveBracketVisitor.java
+++ src/main/java/org/kframework/parser/concrete2kore/disambiguation/RemoveBracketVisitor.java
@@ -1,9 +1,13 @@
 // Copyright (c) 2014-2016 K Team. All Rights Reserved.
 package org.kframework.parser.concrete2kore.disambiguation;
 
+import org.kframework.treeNodes.Ambiguity;
 import org.kframework.treeNodes.SafeTransformer;
 import org.kframework.treeNodes.Term;
 import org.kframework.treeNodes.TermCons;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class RemoveBracketVisitor extends SafeTransformer {
     @Override
@@ -12,5 +16,19 @@ public class RemoveBracketVisitor extends SafeTransformer {
             return apply(tc.get(0));
         }
         return super.apply(tc);
+    }
+
+    // workaround: I would have expected SafeTransformer to eliminate ambiguities with only one node, but apparently it doesn't
+    @Override
+    public Term apply(Ambiguity amb) {
+        Set<Term> trms = new HashSet<>();
+        for (Term t:amb.items()) {
+            trms.add(super.apply(t));
+        }
+
+        if (trms.size() == 1) {
+            return trms.iterator().next();
+        }
+        return super.apply(amb);
     }
 }
